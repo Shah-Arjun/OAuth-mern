44423eb299150cc98ec5294a60d3bf48051863ff
Arjun <arjunsah856@gmail.com>
2026-02-27 04:53:19 +0545

    made forntend get api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,23 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GoogleLogin from "../src/components/GoogleLogin";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
+import {GoogleOAuthProvider} from '@react-oauth/google'
+
+
+  const GoogleAuthWrapper = () => {
+    return (
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <GoogleLogin></GoogleLogin>
+      </GoogleOAuthProvider>
+    )
+  }
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<GoogleLogin />} />
+        <Route path="/login" element={<GoogleAuthWrapper />} />
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/*" element={<PageNotFound />} />
--- frontend/src/components/GoogleLogin.jsx
+++ frontend/src/components/GoogleLogin.jsx
@@ -1,96 +1,88 @@
-import React, { useState } from "react";
+// import React, { useState } from "react"
+import {useGoogleLogin} from '@react-oauth/google'
 
 const GoogleLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("Email:", email);
+  //   console.log("Password:", password);
+  // };
+
+  // function that handle onSuccess, onError
+  const responseGoogle = (authResult) => {
+    try {
+      // if auth code from google cloud
+      if(authResult['code']){
+        
+      }
+    } catch (err) {
+      console.log("Error while requesting google auth-code", err)
+    }
+  }
+
 
   //google login handling
-  const googleLogin = () => {}
+  const googleLogin = useGoogleLogin({
+    onSuccess: responseGoogle,
+    onError: responseGoogle,
+    flow: 'auth-code'   //auth-code form google cloud server
+  })
 
 
   return (
-    <div style={styles.container}>
-      <form style={styles.card} onSubmit={handleSubmit}>
-        <h2 style={styles.title}>Login</h2>
-
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={styles.input}
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={styles.input}
-          required
-        />
-
-        <button type="submit" style={styles.button}>
-          Login
-        </button>
-
-        <button onClick={googleLogin}>Login With Google</button>
-      </form>
+    <div className="App">
+      <button onClick={googleLogin}>Login With Google</button>
     </div>
   );
 };
 
-const styles = {
-  container: {
-    width: "100vw",          // Full width
-    height: "100vh",         // Full height
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f0f4f8",
-  },
-  card: {
-    width: "100%",            // Full width of parent container
-    maxWidth: "400px",        // Limit width so it doesn't stretch too much
-    padding: "40px 30px",
-    borderRadius: "12px",
-    backgroundColor: "#fff",
-    boxShadow: "0 10px 25px rgba(0,0,0,0.1)",
-    display: "flex",
-    flexDirection: "column",
-    gap: "15px",
-  },
-  title: {
-    textAlign: "center",
-    fontSize: "24px",
-    fontWeight: "bold",
-    color: "#1f2937",
-  },
-  input: {
-    padding: "12px",
-    borderRadius: "6px",
-    border: "1px solid #ccc",
-    outline: "none",
-    fontSize: "14px",
-  },
-  button: {
-    marginTop: "10px",
-    padding: "12px",
-    border: "none",
-    borderRadius: "6px",
-    backgroundColor: "#4f46e5",
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-};
+// const styles = {
+//   container: {
+//     width: "100vw",        
+//     height: "100vh",         
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#f0f4f8",
+//   },
+//   card: {
+//     width: "100%",            
+//     maxWidth: "400px",        
+//     padding: "40px 30px",
+//     borderRadius: "12px",
+//     backgroundColor: "#fff",
+//     boxShadow: "0 10px 25px rgba(0,0,0,0.1)",
+//     display: "flex",
+//     flexDirection: "column",
+//     gap: "15px",
+//   },
+//   title: {
+//     textAlign: "center",
+//     fontSize: "24px",
+//     fontWeight: "bold",
+//     color: "#1f2937",
+//   },
+//   input: {
+//     padding: "12px",
+//     borderRadius: "6px",
+//     border: "1px solid #ccc",
+//     outline: "none",
+//     fontSize: "14px",
+//   },
+//   button: {
+//     marginTop: "10px",
+//     padding: "12px",
+//     border: "none",
+//     borderRadius: "6px",
+//     backgroundColor: "#4f46e5",
+//     color: "#fff",
+//     fontWeight: "bold",
+//     fontSize: "16px",
+//     cursor: "pointer",
+//   },
+// };
 
 export default GoogleLogin;
